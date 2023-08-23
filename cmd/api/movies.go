85cbc00909fb21dce2f0fc2f0d0b903d1c47795f
rey <3065588496@qq.com>
2023-08-23 22:17:24 +0100

    customized runtime input message

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -11,10 +11,10 @@ import (
 // Add a createMovieHandler for the "POST /v1/movies" endpoint
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
